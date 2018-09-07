fa5398449e4b02a2e46dfbaf3f6160910f6f4f08
Mitsububunu <36513945+Mitsububunu@users.noreply.github.com>
2018-09-07 17:52:10 +0900

    解答

--- 31/31.py
+++ 31/31.py
@@ -2,16 +2,16 @@ import subprocess
 import re
 import requests
 
-# セッションを接続するための準備
+
 kangacha_url = "http://ctfq.sweetduet.info:10080/~q31/kangacha.php"
 s= requests.Session()
 
-print("一度ポストし、クッキーの情報を得る")
+print("Post then get cookie")
 r = s.post(kangacha_url, data = {"submit":"Gacha"})
 data = s.cookies["ship"]
 signature = s.cookies["signature"]
 
-print("hashpump を subprocess で呼ぶための準備")
+print("Prepare hashpump -> subprocess")
 args = {}
 args["data"] = data
 args["signature"] = signature
@@ -24,21 +24,21 @@ cmd = cmd.format(**args)
 
 print("Popen")
 #stdout=subprocess.PIPE,
-proc = subprocess.Popen(cmd.strip().split(" "),  shell=True)
+proc = subprocess.Popen(cmd.strip().split(" "), stdout=subprocess.PIPE)
 print("proc.communicate()")
 out, err = proc.communicate()
 
-print("得られた cookie を url エンコードにする") 
+print("Encode url with getting cookie") 
 crack_signature, crack_data = out.decode("utf-8").strip().split("\n")
 crack_data = crack_data.replace("\\x","%")
 
-print("cookie を変更して再接続")
+print("Change cookie and re-connaction")
 s.cookies.clear()
 setargs = {"domain":"ctfq.sweetduet.info","path":"/~q31"}
 s.cookies.set("ship",crack_data,**setargs)
 s.cookies.set("signature",crack_signature,**setargs)
 r = s.get(kangacha_url)
 
-print("Yamato がドロップしているので、フラグ部分を抜き出す。")
+print("get from Yamato item ")
 m = re.search("Yamato \[(?P<flag>.*)\]", r.text)
 print(m.group("flag"))
